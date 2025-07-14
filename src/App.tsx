9c187049acc1423704bf9cda7a61346f89e6a96d
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-07-14 11:21:00 +0900

    feat: 로그인 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import { darkTheme, lightTheme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
+import Login from "./pages/Login";
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
     <>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <Header />
+        {/* <Header /> */}
+        <Login />
       </ThemeProvider>
     </>
   );
