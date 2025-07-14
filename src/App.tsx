48e20bd23c1fe9de5ab9698655b4c87996e4c8b3
LJSoo <jisuu7813@naver.com>
2025-07-14 15:23:03 +0900

    feat: 마이페이지 app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import Header from "./components/Header";
 import { darkTheme, lightTheme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import MyPageMain from "./pages/mypage/MyPageMain";
-import { Routes, Route } from "react-router-dom"; // 라우터 임포트
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <>
+    <BrowserRouter>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <Header />
         <Routes>
           <Route path ="/mypage" element={<MyPageMain />} />
         </Routes>
       </ThemeProvider>
+    </BrowserRouter>
     </>
   );
 }
