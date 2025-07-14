29532c275a3477a472cab1367005295f91d19bdd
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-07-14 11:29:17 +0900

    feat: 로그인 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { darkTheme, lightTheme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import Login from "./pages/Login";
@@ -9,8 +10,11 @@ function App() {
   return (
     <>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        {/* <Header /> */}
-        <Login />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
