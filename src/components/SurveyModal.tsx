0af29cc40734824b1ef5c7e03ee4c028d35266ab
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-07-15 10:21:24 +0900

    feat : 메인화면 반응형 구현

--- src/components/SurveyModal.tsx
+++ src/components/SurveyModal.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 
 interface userStateProps {
   setIsNewUser: (value: boolean) => void;
 }
-const ModalContainer = styled.div`
-  width: 50%;
-  height: 75%;
+
+interface styleProps {
+  $ismobile: boolean;
+}
+const ModalContainer = styled.div<styleProps>`
+  width: ${(props) => (props.$ismobile ? "90%" : "50%")};
+  height: ${(props) => (props.$ismobile ? "85%" : "75%")};
   top: 50%;
   left: 50%;
   border-radius: 8px;
   transform: translate(-50%, -50%);
-  position: absolute;
+  position: fixed;
   /* display: flex;
   justify-content: center;
   align-items: center; */
-  background-color: rgba(0, 0, 0, 0.9);
+  background-color: rgba(0, 0, 0, 0.7);
   z-index: 3100;
 `;
 
@@ -41,8 +46,8 @@ const SkipBtn = styled.button`
   cursor: pointer;
 `;
 
-const MainText = styled.span`
-  font-size: 1.9rem;
+const MainText = styled.span<styleProps>`
+  font-size: ${(props) => (props.$ismobile ? "1.5rem" : "1.9rem")};
   font-weight: 600;
   color: white;
   /* font-family: sans-serif; */
@@ -54,15 +59,16 @@ const SubText = styled.span`
   color: white;
 `;
 
-const GenreBtn = styled.button<{ $selected: boolean }>`
-  width: 70px;
-  height: 35px;
+const GenreBtn = styled.button<{ $selected: boolean; $ismobile: boolean }>`
+  width: ${(props) => (props.$ismobile ? "60px" : "70px")};
+  height: ${(props) => (props.$ismobile ? "35px" : "35px")};
   text-align: center;
   font-size: 0.8rem;
   background-color: ${({ $selected }) => ($selected ? "#FE5890" : "#d9d9d9")};
   border: none;
   border-radius: 14px;
   color: black;
+  transition: background-color 0.2s ease;
   cursor: pointer;
   &:hover {
     background-color: pink;
@@ -78,9 +84,34 @@ const GenreContainer = styled.div`
   height: 35px;
 `;
 
+const ConfirmBtn = styled.button<{ $isbtnpos: boolean; $ismobile: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => (props.$ismobile ? "80px" : "100px")};
+  height: 30px;
+  margin-top: 30px;
+  background-color: ${(props) => (props.$isbtnpos ? "#fa5a8e" : "#5f5d5d")};
+  color: ${(props) => (props.$isbtnpos ? "black" : "white")};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => (props.$isbtnpos ? "#e04a78" : "#5f5d5d")};
+  }
+
+  &:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
+
 const SurveyModal = ({ setIsNewUser }: userStateProps) => {
   const [username, setUsername] = useState("권오윤");
   const [selectedGenre, setSelectedGenre] = useState<string[]>([]);
+  const [isBtnPos, setIsBtnPos] = useState(false);
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   const genreGroups = [
     ["코미디", "로맨스", "스릴러"],
     ["공포", "재난", "범죄", "좀비", "역사"],
@@ -94,13 +125,18 @@ const SurveyModal = ({ setIsNewUser }: userStateProps) => {
   };
 
   useEffect(() => {
-    console.log(selectedGenre);
+    if (selectedGenre.length >= 1) {
+      setIsBtnPos(true);
+    } else {
+      setIsBtnPos(false);
+    }
   }, [selectedGenre]);
+
   return (
-    <ModalContainer>
+    <ModalContainer $ismobile={isMobile}>
       <Modal>
-        <MainText>안녕하세요. {username}님 </MainText>
-        <MainText style={{ marginTop: "10px" }}>
+        <MainText $ismobile={isMobile}>안녕하세요. {username}님 </MainText>
+        <MainText $ismobile={isMobile} style={{ marginTop: "10px" }}>
           KINO에 오신 것을 환영합니다!
         </MainText>
         <SubText style={{ marginTop: "50px" }}>
@@ -111,6 +147,7 @@ const SurveyModal = ({ setIsNewUser }: userStateProps) => {
         </SubText>
         <GenreBtn
           style={{ marginTop: "50px" }}
+          $ismobile={isMobile}
           $selected={selectedGenre.includes("SF")}
           onClick={() => handleGenreClick("SF")}
         >
@@ -121,6 +158,7 @@ const SurveyModal = ({ setIsNewUser }: userStateProps) => {
             {group.map((genre) => (
               <GenreBtn
                 key={genre}
+                $ismobile={isMobile}
                 $selected={selectedGenre.includes(genre)}
                 onClick={() => handleGenreClick(genre)}
               >
@@ -129,7 +167,16 @@ const SurveyModal = ({ setIsNewUser }: userStateProps) => {
             ))}
           </GenreContainer>
         ))}
-
+        <ConfirmBtn
+          $isbtnpos={isBtnPos}
+          $ismobile={isMobile}
+          disabled={!isBtnPos}
+          onClick={() => {
+            console.log("clicked");
+          }}
+        >
+          확인
+        </ConfirmBtn>
         <SkipBtn
           onClick={() => {
             setIsNewUser(false);
